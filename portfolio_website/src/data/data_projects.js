b07fef53ab7713d5a1b50aaba15f06ce854af7a4
Max Moeller <maximomoeller@gmail.com>
2026-06-06 20:29:27 +1000

    project added : File Organizer

--- portfolio_website/src/data/data_projects.js
+++ portfolio_website/src/data/data_projects.js
@@ -5,6 +5,8 @@ const projects = [
       "Full-stack real-time messaging platform featuring JWT authentication, Socket.IO communication, REST APIs and MongoDB persistence.",
     image: "/images/chat-app.jpg",
     tech: ["React", "Express", "MongoDB", "Socket.IO"],
+    github: "https://github.com/maxmoeller-147/MERN-Project",
+    live: null,
   },
   {
     title: "Comic Store API",
@@ -12,12 +14,27 @@ const projects = [
       "Backend API built with Flask and PostgreSQL to manage business operations through scalable database relationships and CRUD endpoints.",
     image: "/images/comic-store.jpg",
     tech: ["Python", "Flask", "PostgreSQL"],
+    github: "https://github.com/maxmoeller-147/API_Server_Comics",
+    live: "https://api-server-comics.onrender.com/comics/",
   },
+
+  {
+    title: "Python File Organizer",
+    description:
+        "File management automation tool that categorizes and moves files based on extension types. Features customizable file categories, case-insensitive matching, safe handling of unsupported files and a modular architecture for future expansion.",
+    image: "/images/file-organizer.jpg",
+    tech: ["Python", "Pathlib", "Shutil"],
+    github: "https://github.com/maxmoeller-147/file_organizer",
+    live: null,
+  },
+
   {
     title: "Flesh & Blood Deck Builder",
     description:
       "CLI application designed to streamline deck creation with matchup recommendations, utility tools and database-driven card management.",
     image: "/images/deck-builder.jpg",
     tech: ["Python", "SQLite"],
+    github: "https://github.com/maxmoeller-147/Terminal_App",
+    live: null,
   },
 ]
